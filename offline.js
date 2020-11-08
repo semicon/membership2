433bdf8d2b9c307c571b158f1cc9ca3375afae01
guruchian <1210@sts.ac.th>
2020-11-08 09:20:18 +0700

    membership2

---
+++ offline.js
@@ -0,0 +1,17 @@
+﻿{
+	"version": 1604763022,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bar.png",
+		"images/logo-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
